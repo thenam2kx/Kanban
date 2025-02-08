f760b6ea5c02f257e62399c12f35c2dc48b6aae2
thenam2kx <thenam2kx@gmail.com>
2025-02-08 16:53:13 +0700

    setup basic

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import theme from '@/themes/theme.ts';
 import { BrowserRouter } from "react-router";
+import { CssBaseline } from '@mui/material'
+import { ThemeProvider } from '@mui/material/styles'
+import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
 )
